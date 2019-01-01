f898e74e220244a390f1ee498739d45d3fdc7ea2
Sonvendra Chaurasia <sonvendra@gmail.com>
2019-01-01 09:43:29 +0530

    Add files via upload

--- git/js/custom.js
+++ git/js/custom.js
@@ -39,3 +39,26 @@ win.scroll(function(event) {
 
 });
 
+
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        element1.classList.remove("down-remove");
+        element2.classList.remove("down-remove");
+        element3.classList.remove("down-remove");
+        element4.classList.remove("down-remove");
+        element5.classList.remove("down-remove");
+        element6.classList.remove("down-remove");
+    } else {
+        console.log("Media Query Not Matches")
+    }
+}
+var element1 = document.getElementById("cRemove1");
+var element2 = document.getElementById("cRemove2");
+var element3 = document.getElementById("cRemove3");
+var element4 = document.getElementById("cRemove4");
+var element5 = document.getElementById("cRemove5");
+var element6 = document.getElementById("cRemove6");
+var x = window.matchMedia("(max-width: 800px)");
+myFunction(x);// Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
